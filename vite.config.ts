c7c932bda24be85f6a872a73b2e64f29c70237a4
Omnitual <core.omnibox@gmail.com>
2025-12-22 21:06:36 -0700

    fix v3

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true
+  },
+  preview: {
+    host: true,
+    port: 4173,
+    allowedHosts: true
+  }
 })
